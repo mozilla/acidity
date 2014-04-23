8b58bfed73d52a8526a89c6bc479fd931ffa2026
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-04-23 15:17:13 -0400

    Fix whitespace

--- src/tests/number-of-files.js
+++ src/tests/number-of-files.js
@@ -13,17 +13,16 @@ function test(workingDirectory) {
     });
 
     if (fileListLength < 8) {
-      return {
-          message: 'Not too many files on the root directory',
-          passed: true
-      };
+        return {
+            message: 'Not too many files on the root directory',
+            passed: true
+        };
     }
 
     return {
-      message: 'Too many files on the root directory: ' + fileListLength + '. Cognitive overload detected!',
+        message: 'Too many files on the root directory: ' + fileListLength + '. Cognitive overload detected!',
         passed: false
     };
-  
 }
 
 module.exports = test;
