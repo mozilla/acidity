1f806f08329719e36c9467f94b91653e3f1d55c9
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-04-23 14:08:57 -0400

    Add folders test (fix #4)

--- src/acidity.js
+++ src/acidity.js
@@ -1,8 +1,10 @@
+var foldersTest = require('./tests/folders.js');
 var licenseTest = require('./tests/license.js');
 var readmeTest = require('./tests/readme.js');
 var numberOfFilesTest = require('./tests/number-of-files.js');
 
 module.exports = {
+    folders: foldersTest,
     license: licenseTest,
     readme: readmeTest,
 	numberOfFiles: numberOfFilesTest
---
+++ src/tests/folders.js
@@ -0,0 +1,30 @@
+var shell = require('shelljs');
+
+var REQUIRED_FOLDERS = {
+    dist: ["dist"],
+    examples: ["examples"],
+    src: ["src"],
+    test: ["test", "tests"]
+};
+
+function test(workingDirectory) {
+    shell.cd(workingDirectory);
+
+    for (var i in REQUIRED_FOLDERS) {
+        for (var option in REQUIRED_FOLDERS[i]) {
+            if (shell.test('-d', REQUIRED_FOLDERS[i][option])) {
+                return {
+                    message: '"' + i + '" folder exists!',
+                    passed: true
+                };
+            } else {
+                return {
+                    message: '"' + i + '" folder does not exist!',
+                    passed: false
+                };
+            }
+        }
+    }
+}
+
+module.exports = test;
