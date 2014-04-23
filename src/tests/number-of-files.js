3df13c726a7911506434077789f5208503daad6b
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-04-23 15:15:42 -0400

    Only count files, not folders

--- src/tests/number-of-files.js
+++ src/tests/number-of-files.js
@@ -3,22 +3,27 @@ var shell = require('shelljs');
 function test(workingDirectory) {
     shell.cd(workingDirectory);
 
-    var fileListLength = shell.ls(workingDirectory).length;
+    var fileListLength = 0;
 
-    if(fileListLength < 8) {
+    shell.ls(workingDirectory).forEach(function(file) {
+        // Only count files, not folders.
+        if (shell.test('-f', file)) {
+            fileListLength++;
+        }
+    });
 
+    if (fileListLength < 8) {
       return {
           message: 'Not too many files on the root directory',
           passed: true
       };
-
     }
 
     return {
       message: 'Too many files on the root directory: ' + fileListLength + '. Cognitive overload detected!',
         passed: false
     };
-	
+  
 }
 
 module.exports = test;
